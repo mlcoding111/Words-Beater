cae12b72a3dfda726f55ce865d2f2dbf2c3e0f6f
mlcoding111 <kennycodes@hotmail.com>
2022-06-08 16:57:30 -0400

    Last commit before combining the back-end and front-end together

--- server/server.js
+++ server/server.js
@@ -3,9 +3,9 @@ const app = express()
 const path = require("path");
 const helmet = require('helmet')
 
+// Middleware
 app.use(helmet())
-
-// add middleware
+app.use(logger)
 app.use(express.static("public"));
 
 const userRouter = require("./routes/users")
@@ -13,4 +13,19 @@ const userRouter = require("./routes/users")
 app.use('/users', userRouter);
 
 
+function logger(req, res, next){
+    // Get url every time page load
+    console.log(req.originalUrl)
+    // then continue to the next code..
+    next()
+}
+
 app.listen(3000)
+
+
+
+// We can use middleware for a single request, for example..
+// app.get("/", logger, (req,res) => {
+//     res.send("Logger")
+// })
+// We can also call it on top of a route and every routes will call it
