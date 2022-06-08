531e2a6fab30e1242970864016237b14b125d0d3
mlcoding111 <kennycodes@hotmail.com>
2022-06-08 14:26:25 -0400

    Changed users route structure to make it cleaner

--- server/routes/users.js
+++ server/routes/users.js
@@ -5,21 +5,20 @@ router.get("/", (req, res) => {
     res.send("Users List")
 })
 
-router.get("/:id", (req, res) => {
-    let userId = req.params.id
-    res.send(`Get user with id ${userId}`)
-})
-
-router.put("/:id", (req, res) => {
-    let userId = req.params.id
-    res.send(`Update user with id ${userId}`)
-})
-
-
-router.delete("/:id", (req, res) => {
-    let userId = req.params.id
-    res.send(`Delete user with id ${userId}`)
-})
+router
+    .route("/:id")
+    .get((req, res) => {
+        let userId = req.params.id
+        res.send(`Get user with id ${userId}`)
+    })
+    .put(("/:id", (req, res) => {
+        let userId = req.params.id
+        res.send(`Update user with id ${userId}`)
+    }))
+    .delete(("/:id", (req, res) => {
+        let userId = req.params.id
+        res.send(`Delete user with id ${userId}`)
+    }))
 
 
 
