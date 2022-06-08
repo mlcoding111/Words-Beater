6e598a962dcf9da31198a14ccc85c0d6629ce2b1
mlcoding111 <kennycodes@hotmail.com>
2022-06-08 14:32:31 -0400

    Added router.param function to run when we get {id} param

--- server/routes/users.js
+++ server/routes/users.js
@@ -8,18 +8,23 @@ router.get("/", (req, res) => {
 router
     .route("/:id")
     .get((req, res) => {
-        let userId = req.params.id
-        res.send(`Get user with id ${userId}`)
+        res.send(`Get user with id ${req.params.id} and name: ${req.user.name}`)
     })
     .put(("/:id", (req, res) => {
-        let userId = req.params.id
-        res.send(`Update user with id ${userId}`)
+        res.send(`Update user with id ${req.params.id}`)
     }))
     .delete(("/:id", (req, res) => {
-        let userId = req.params.id
-        res.send(`Delete user with id ${userId}`)
+        res.send(`Delete user with id ${req.params.id}`)
     }))
 
+const users = [{name: "Mic"}, { name: 'Kenny'} ]
 
+// Whenever we get a param {id}, run this function
+// Every time we get a {id}, we set the req.user to the user matchin the id, then we can call it anywhere inside users.js
+router.param("id", (req, res, next, id) => {
+    req.user = users[id]
+    // Go to the next piece of middleware (prevent infinite loading)
+    next()
+})
 
 module.exports = router
