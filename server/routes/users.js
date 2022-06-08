f2b7157a38046470b8c9e6d0bef8240350a0746a
mlcoding111 <kennycodes@hotmail.com>
2022-06-08 14:23:41 -0400

    Added get,put,delete route for users

--- server/routes/users.js
+++ server/routes/users.js
@@ -5,9 +5,22 @@ router.get("/", (req, res) => {
     res.send("Users List")
 })
 
-router.post("/", (req, res) => {
-    res.send("Post")
+router.get("/:id", (req, res) => {
+    let userId = req.params.id
+    res.send(`Get user with id ${userId}`)
 })
+
+router.put("/:id", (req, res) => {
+    let userId = req.params.id
+    res.send(`Update user with id ${userId}`)
+})
+
+
+router.delete("/:id", (req, res) => {
+    let userId = req.params.id
+    res.send(`Delete user with id ${userId}`)
+})
+
 
 
 module.exports = router
